24755fe78f6bf6f258c3182b8a42eed2abffbbfc
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-07-22 22:57:05 +0300

    Create ExcellentNumber.java

---
+++ ExcellentNumber.java
@@ -0,0 +1,24 @@
+package excellentnumber;
+
+public class ExcellentNumber {
+	public static void main(String[] args) {
+		
+		int number = 28;
+		int total = 0;
+		
+		for(int i =1;i<number;i++) {
+			if(number%i == 0) {
+				total = total+i;
+			}
+		}
+		
+		if(total == number) {
+			System.out.println("This number is excellent ");
+			
+		}
+		else {
+			System.out.println("This number is not excellent");
+		}
+	}
+
+}
